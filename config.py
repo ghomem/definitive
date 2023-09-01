121c2d898275c7146a82b133842227197353f70d
Gustavo Homem <homem.gustavo@gmail.com>
2023-09-01 17:13:15 +0200

    have scaling factor as a paramter

--- config.py
+++ config.py
@@ -14,33 +14,18 @@ E_ERR = 1
 # * the definition
 # * the "see also" section
 
-# this constant allows for adding more vertical space, which is necessary when the definition is long
-# this size refers to the situation where SCALING_FACTOR == 1
-BASE_HEIGHT = 714
-
-# this constant allows for fine tuning the resulting image size keeping proportions
-SCALING_FACTOR = 1
-
 # in principle the remaining constants do not need to be adjusted
-WIDTH  = round(890 * SCALING_FACTOR)
-HEIGHT = round(BASE_HEIGHT * SCALING_FACTOR)
+WIDTH  = 890
+HEIGHT = 714
 
-X_PADDING = round(40 * SCALING_FACTOR)
-Y_PADDING = round(20 * SCALING_FACTOR)
-Y_PADDING2 = round(40 * SCALING_FACTOR)
+X_PADDING = 40
+Y_PADDING = 20
+Y_PADDING2 = 40
 
 X_SPACING = 0
 
-X_WORD_TO_DEFINE = 0 + X_PADDING
-Y_WORD_TO_DEFINE = 0 + Y_PADDING
-
-X_WORD_DEFINITION = 0 + X_PADDING
-X_WORD_TYPE = 0 + X_PADDING
-
-X_SEE_ALSO = 0 + X_PADDING
-
 WORD_DEFINITION_LINE_WIDTH = 50
-WORD_DEFINITION_LINE_SPACING = round(10 * SCALING_FACTOR)
+WORD_DEFINITION_LINE_SPACING = 10
 
 ### COLOR RELATED DEFINITIONS ###
 
@@ -66,11 +51,6 @@ FONT_WORD_TYPE_BASE = font_manager.findfont(FONT_WORD_TYPE_PROPS)
 FONT_WORD_DEFINITION_BASE = font_manager.findfont(FONT_WORD_DEFINITION_PROPS)
 
 # then we pick the font sizes
-FONT_SIZE_WORD_TO_DEFINE = round(100 * SCALING_FACTOR)
-FONT_SIZE_WORD_TYPE = round(60 * SCALING_FACTOR)
-FONT_SIZE_WORD_DEFINITION = round(36 * SCALING_FACTOR)
-
-# and finally we instantiate the fonts
-FONT_WORD_TO_DEFINE = ImageFont.truetype(FONT_WORD_TO_DEFINE_BASE, size=FONT_SIZE_WORD_TO_DEFINE)
-FONT_WORD_TYPE = ImageFont.truetype(FONT_WORD_TYPE_BASE, size=FONT_SIZE_WORD_TYPE)
-FONT_WORD_DEFINITION = ImageFont.truetype(FONT_WORD_DEFINITION_BASE, size=FONT_SIZE_WORD_DEFINITION)
+FONT_SIZE_WORD_TO_DEFINE = 100
+FONT_SIZE_WORD_TYPE = 60
+FONT_SIZE_WORD_DEFINITION = 36
--- definitive.py
+++ definitive.py
@@ -4,19 +4,21 @@ import argparse
 from matplotlib import font_manager
 from PIL import Image, ImageDraw, ImageFont
 
-from config import ( E_OK, E_ERR, WIDTH, HEIGHT, X_WORD_TO_DEFINE, X_WORD_TYPE, X_WORD_DEFINITION, X_SEE_ALSO,
-                     Y_WORD_TO_DEFINE, Y_PADDING, Y_PADDING2, BG_COLOR, FONT_COLOR, WORD_DEFINITION_LINE_WIDTH,
-                     FONT_WORD_TO_DEFINE, FONT_WORD_TYPE, FONT_WORD_DEFINITION, WORD_DEFINITION_LINE_SPACING )
+from config import ( E_OK, E_ERR, WIDTH, HEIGHT, X_PADDING, Y_PADDING, Y_PADDING2, BG_COLOR, FONT_COLOR,
+                     FONT_SIZE_WORD_TO_DEFINE, FONT_SIZE_WORD_TYPE, FONT_SIZE_WORD_DEFINITION,
+                     FONT_WORD_TO_DEFINE_BASE, FONT_WORD_TYPE_BASE, FONT_WORD_DEFINITION_BASE,
+                     WORD_DEFINITION_LINE_WIDTH, WORD_DEFINITION_LINE_SPACING )
 
 
 def main():
 
     parser = argparse.ArgumentParser(description='Generate a dictionary entry on a png file.')
-    parser.add_argument( '-w', '--word',        required=True, help='Word to be defined')
-    parser.add_argument( '-c', '--word-class',  required=True, help='Word class, such as noun, adjective, verb, etc')
-    parser.add_argument( '-d', '--definition',  required=True, help='The definition of the word')
-    parser.add_argument( '-s', '--see-also',    required=True, help='Other related words')
-    parser.add_argument( '-o', '--output-file', required=True, help='Other related words')
+    parser.add_argument( '-w', '--word',           required=True,  help='Word to be defined')
+    parser.add_argument( '-c', '--word-class',     required=True,  help='Word class, such as noun, adjective, verb, etc')
+    parser.add_argument( '-d', '--definition',     required=True,  help='The definition of the word')
+    parser.add_argument( '-s', '--see-also',       required=True,  help='Other related words')
+    parser.add_argument( '-o', '--output-file',    required=True,  help='Other related words')
+    parser.add_argument( '-f', '--scaling-factor', required=False, help='Scaling factor for the generated image', type=int, default=1)
 
     args = parser.parse_args()
 
@@ -25,37 +27,70 @@ def main():
     word_definition = args.definition
     see_also = args.see_also
     output_file = args.output_file
+    scaling_factor = args.scaling_factor
+
+    # this scaling_factor parameter allows for fine tuning the resulting image size keeping proportion
+
+    width  = round(WIDTH * scaling_factor)
+    height = round(HEIGHT * scaling_factor)
+
+    x_padding = round(X_PADDING * scaling_factor)
+    y_padding = round(Y_PADDING * scaling_factor)
+    y_padding2 = round(Y_PADDING2 * scaling_factor)
+
+    x_word_to_define = 0 + x_padding
+    y_word_to_define = 0 + y_padding
+
+    x_word_definition = 0 + x_padding
+    x_word_type = 0 + x_padding
+
+    x_see_also = 0 + x_padding
+
+    word_definition_line_spacing = round(WORD_DEFINITION_LINE_SPACING * scaling_factor)
+
+    # we do not scale this one because we are scaling the characters themselves
+    word_definition_line_width = WORD_DEFINITION_LINE_WIDTH
+
+    # here we pick the font sizes
+    font_size_word_to_define = round(FONT_SIZE_WORD_TO_DEFINE * scaling_factor)
+    font_size_word_type = round(FONT_SIZE_WORD_TYPE * scaling_factor)
+    font_size_word_definition = round(FONT_SIZE_WORD_DEFINITION * scaling_factor)
+
+    # and here we instantiate the fonts
+    font_word_to_define = ImageFont.truetype(FONT_WORD_TO_DEFINE_BASE, size=font_size_word_to_define)
+    font_word_type = ImageFont.truetype(FONT_WORD_TYPE_BASE, size=font_size_word_type)
+    font_word_definition = ImageFont.truetype(FONT_WORD_DEFINITION_BASE, size=font_size_word_definition)
 
     ### MAIN SCRIPT ###
 
     word_class_str = f"({word_class})"
     see_also_str = f"(see also: {see_also})"
 
-    word_definition_lines = textwrap.wrap(word_definition, width=WORD_DEFINITION_LINE_WIDTH)
+    word_definition_lines = textwrap.wrap(word_definition, width=word_definition_line_width)
 
-    img = Image.new('RGB', (WIDTH, HEIGHT), color=BG_COLOR)
+    img = Image.new('RGB', (width, height), color=BG_COLOR)
 
     draw = ImageDraw.Draw(img)
 
-    y_spacing = draw.textsize(word_to_define, font=FONT_WORD_TO_DEFINE)[1] + Y_PADDING
+    y_spacing = draw.textsize(word_to_define, font=font_word_to_define)[1] + y_padding
 
-    y_word_class = Y_WORD_TO_DEFINE + y_spacing
+    y_word_class = y_word_to_define + y_spacing
 
-    y_spacing2 = draw.textsize(word_class, font=FONT_WORD_TO_DEFINE)[1] + Y_PADDING2
+    y_spacing2 = draw.textsize(word_class, font=font_word_to_define)[1] + y_padding2
 
     y_word_definition = y_word_class + y_spacing2
 
-    draw.text((X_WORD_TO_DEFINE, Y_WORD_TO_DEFINE), word_to_define, font=FONT_WORD_TO_DEFINE, fill=FONT_COLOR)
-    draw.text((X_WORD_TYPE, y_word_class), word_class_str, font=FONT_WORD_TYPE, fill=FONT_COLOR)
+    draw.text((x_word_to_define, y_word_to_define), word_to_define, font=font_word_to_define, fill=FONT_COLOR)
+    draw.text((x_word_type, y_word_class), word_class_str, font=font_word_type, fill=FONT_COLOR)
 
     y_pos = y_word_definition
     for line in word_definition_lines:
-        draw.multiline_text((X_WORD_DEFINITION, y_pos), line, font=FONT_WORD_DEFINITION, fill=FONT_COLOR)
-        y_pos = y_pos + draw.textsize(line, font=FONT_WORD_DEFINITION)[1] + WORD_DEFINITION_LINE_SPACING
+        draw.multiline_text((x_word_definition, y_pos), line, font=font_word_definition, fill=FONT_COLOR)
+        y_pos = y_pos + draw.textsize(line, font=font_word_definition)[1] + word_definition_line_spacing
 
-    y_pos = y_pos + 2 * Y_PADDING
+    y_pos = y_pos + 2 * y_padding
 
-    draw.text((X_SEE_ALSO, y_pos), see_also_str, font=FONT_WORD_DEFINITION, fill=FONT_COLOR)
+    draw.text((x_see_also, y_pos), see_also_str, font=font_word_definition, fill=FONT_COLOR)
 
     try:
         img.save(output_file)
